b42096017407f45efa92dc3811aac80bd438fa82
t-mangoe <m.takashi.xpair@gmail.com>
2021-02-27 15:04:04 +0900

    made text fields in a dialog.

--- src/components/CreateMemoButton.vue
+++ src/components/CreateMemoButton.vue
@@ -12,26 +12,33 @@
       Open Dialog
     </v-btn>
 
-    <v-dialog v-model="dialog" max-width="290">
+    <v-dialog v-model="dialog" max-width="75%">
       <v-card>
-        <v-card-title class="headline"
-          >Use Google's location service?</v-card-title
-        >
-
-        <v-card-text>
-          Let Google help apps determine location. This means sending anonymous
-          location data to Google, even when no apps are running.
-        </v-card-text>
+        <v-card-title class="headline">メモを作成</v-card-title>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field label="タイトル" v-model="titleText"></v-text-field>
+            </v-col>
+            <v-col cols="12">
+              <!-- marerializeのCSSと競合しているので、rowsはうまく反映されない -->
+              <v-textarea
+                label="内容"
+                value=""
+                rows="10"
+                v-model="contentsText"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+        </v-container>
 
         <v-card-actions>
           <v-spacer></v-spacer>
-
           <v-btn color="green darken-1" text @click="dialog = false">
-            Disagree
+            Cancel
           </v-btn>
-
           <v-btn color="green darken-1" text @click="dialog = false">
-            Agree
+            Create
           </v-btn>
         </v-card-actions>
       </v-card>
@@ -51,6 +58,8 @@ export default {
   data() {
     return {
       dialog: false,
+      titleText: "",
+      contentsText: "",
     };
   },
 };
