e3ea35656161dd650cbcfc3662fd28ea0fe4d2c8
t-mangoe <m.takashi.xpair@gmail.com>
2021-02-27 09:47:03 +0900

    introduce vuetify.

--- src/App.vue
+++ src/App.vue
@@ -7,34 +7,22 @@
 </template>
 
 <script>
-// import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from './components/HelloWorld';
 import CardArea from "./components/CardArea.vue";
 import PreLoader from "./components/PreLoader.vue";
 import CreateMemoButton from "./components/CreateMemoButton.vue";
-
 export default {
   name: "App",
+
   components: {
     // HelloWorld,
     PreLoader,
     CardArea,
     CreateMemoButton,
   },
-  data() {
-    return {
-      PreLoaderShow: false,
-    };
-  },
+
+  data: () => ({
+    PreLoaderShow: false,
+  }),
 };
 </script>
-
-<style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
 // import axios from 'axios'
 
 Vue.config.productionTip = false
@@ -9,5 +10,6 @@ Vue.config.productionTip = false
 // });
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
