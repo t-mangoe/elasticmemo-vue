8d1fb12f119a0ad28b38b739e5c46e9a79925c66
t-mangoe <m.takashi.xpair@gmail.com>
2021-03-19 17:31:38 +0900

    add codes for editing a memo.

--- src/components/MemoCard.vue
+++ src/components/MemoCard.vue
@@ -1,8 +1,8 @@
 <template>
   <v-container>
     <v-card class="mx-auto" outlined @click.stop="dialog = true">
-      <v-card-title class="headline">{{ title }}</v-card-title>
-      <v-card-text>{{ message }}</v-card-text>
+      <v-card-title class="headline">{{ editingTitle }}</v-card-title>
+      <v-card-text>{{ editingMessage }}</v-card-text>
     </v-card>
 
     <v-dialog v-model="dialog" max-width="75%">
@@ -11,14 +11,17 @@
         <v-container>
           <v-row>
             <v-col cols="12">
-              <v-text-field label="タイトル" v-model="title"></v-text-field>
+              <v-text-field
+                label="タイトル"
+                v-model="editingTitle"
+              ></v-text-field>
             </v-col>
             <v-col cols="12">
               <v-textarea
                 label="内容"
                 value=""
                 rows="10"
-                v-model="message"
+                v-model="editingMessage"
               ></v-textarea>
             </v-col>
           </v-row>
@@ -26,7 +29,7 @@
 
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn color="green darken-1" text @click="dialog = false">
+          <v-btn color="green darken-1" text @click="updateMemo">
             Update
           </v-btn>
           <v-btn color="green darken-1" text @click="deleteMemo">
@@ -61,6 +64,30 @@ export default {
 
       this.dialog = false;
     },
+    updateMemo() {
+      console.log("update memo.");
+      const date = new Date();
+      const dateStr = date.toISOString();
+      const url =
+        "http://192.168.11.50:9200/my_index/my_type/" + this.id + "?pretty";
+      const title = this.title;
+      const message = this.message;
+
+      axios
+        .put(url, {
+          user_name: "user",
+          date: dateStr,
+          title,
+          message,
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+      this.dialog = false;
+    },
   },
   props: {
     id: {
@@ -79,8 +106,8 @@ export default {
   data() {
     return {
       dialog: false,
-      titleText: "",
-      contentsText: "",
+      editingTitle: this.title,
+      editingMessage: this.message,
     };
   },
 };
