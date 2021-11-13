d06bfb232412e55583f1971027baa73b74831a96
t-mangoe <m.takashi.xpair@gmail.com>
2021-11-13 09:32:48 +0900

    import TagAdministrator in global

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import TagAdministrator from "./components/TagAdministrator.js";
 // import axios from 'axios'
 
-Vue.config.productionTip = false
-
+Vue.config.productionTip = false;
+Vue.prototype.$tagAdmin = new TagAdministrator();
 // document.addEventListener('DOMContentLoaded', function () {
 //   M.AutoInit();
 // });
