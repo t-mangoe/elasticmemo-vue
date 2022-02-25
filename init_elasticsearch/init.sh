c6b574460934a0a992eb934b019c7dfa5ca51ffb
t-mangoe <m.takashi.xpair@gmail.com>
2022-02-26 08:51:19 +0900

    add tag init request in init.sh

--- init_elasticsearch/init.sh
+++ init_elasticsearch/init.sh
@@ -1,1 +1,2 @@
 curl -X POST http://localhost:9200/my_index/my_type?pretty -H "Content-Type":"application/json" -d @data.json
+curl -X POST http://localhost:9200/tags/tag_type?pretty -H "Content-Type":"application/json" -d @init_tags.json
