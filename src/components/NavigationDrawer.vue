24074428731a721ae2ead617d0259f41b3b62483
t-mangoe <m.takashi.xpair@gmail.com>
2022-06-25 09:54:23 +0900

    added process to remove tags

--- src/components/NavigationDrawer.vue
+++ src/components/NavigationDrawer.vue
@@ -10,11 +10,11 @@
 
         <v-list-item
           v-for="tag in tags"
-          :key="tag"
+          :key="tag.id"
           @click="narrowTag(tag, $event)"
         >
           <v-list-item-content>
-            <v-list-item-title>{{ tag }}</v-list-item-title>
+            <v-list-item-title>{{ tag.name }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
 
@@ -37,12 +37,12 @@
             </v-col>
           </v-row>
 
-          <v-row v-for="tag in tags" :key="tag">
+          <v-row v-for="tag in tags" :key="tag.id">
             <v-col md="auto">
-              <v-icon>mdi-delete</v-icon>
+              <v-icon @click="deleteTag(tag)">mdi-delete</v-icon>
             </v-col>
             <v-col>
-              {{ tag }}
+              {{ tag.name }}
             </v-col>
             <v-col md="auto">
               <v-icon>mdi-pencil</v-icon>
@@ -84,18 +84,7 @@ export default {
     tagAdmin.echo();
     // this.tagAdmin = TagAdministrator.hoge();
 
-    axios
-      .post("es/tags/_search")
-      .then((response) => {
-        console.log("通信成功！！");
-        console.log(response);
-        const tagData = response.data.hits.hits;
-        this.tags = tagData.map((data) => data._source.tagName);
-      })
-      .catch((error) => {
-        console.log("通信失敗！！");
-        console.log(error);
-      });
+    this.getAndUpdateTags();
   },
   methods: {
     open() {
@@ -113,6 +102,7 @@ export default {
         .then((response) => {
           console.log(response);
           confirm("タグの追加に成功！！");
+          this.getAndUpdateTags();
         })
         .catch((error) => {
           console.error(error);
@@ -123,9 +113,41 @@ export default {
     },
     narrowTag(tag, event) {
       console.log(event);
-      confirm("narrow tag: " + tag);
+      confirm("narrow tag: " + tag.name);
       // 検索と同様に処理する。App.vue経由でCardArea.vueに処理を移す
-      this.$emit("tag-narrow-event", tag);
+      this.$emit("tag-narrow-event", tag.name);
+    },
+    deleteTag(tag) {
+      confirm("タグを削除します。ID:" + tag.id);
+      axios
+        .delete("es/tags/tag_type/" + tag.id)
+        .then((response) => {
+          console.log(response);
+          confirm("タグの削除に成功");
+          this.getAndUpdateTags();
+        })
+        .catch((error) => {
+          console.error(error);
+          confirm("タグの削除に失敗");
+        });
+    },
+    getAndUpdateTags() {
+      this.tags = [];
+      axios
+        .post("es/tags/_search")
+        .then((response) => {
+          console.log("通信成功！！");
+          console.log(response);
+          const tagData = response.data.hits.hits;
+          this.tags = tagData.map((data) => ({
+            id: data._id,
+            name: data._source.tagName,
+          }));
+        })
+        .catch((error) => {
+          console.log("通信失敗！！");
+          console.log(error);
+        });
     },
   },
 };
