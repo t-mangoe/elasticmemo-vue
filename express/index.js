84655a10e40906a08e2e174cef72ef6d8cb95ce6
t-mangoe <m.takashi.xpair@gmail.com>
2023-03-04 09:20:28 +0900

    add get-memo method

--- express/index.js
+++ express/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const axiosBase = require("axios");
+const axios = axiosBase.create({
+  baseURL: "http://es:9200/",
+});
 
 // expressアプリを生成する
 const app = express();
@@ -6,6 +10,34 @@ const app = express();
 // 動作確認用の処理
 app.get("/", function (req, res) {
   res.send("ページが表示されました！");
+});
+
+// TODO: ElasticSearchとの通信処理を作成する
+
+axios.defaults.base;
+
+// メモを取得する処理
+app.get("/get-memo", function (req, res) {
+  axios
+    .post("/my_index/_search", {
+      sort: [
+        {
+          date: "desc",
+        },
+      ],
+    })
+    .then((response) => {
+      console.log("ElasticSearchからのデータ取得に成功しました");
+      console.log(response);
+      const data = response.data;
+      res.send(JSON.stringify(data));
+    })
+    .catch(function (error) {
+      console.error("ElasticSearchからのデータ取得に失敗！！");
+      console.error(error);
+      res.status(500);
+      res.send(error);
+    });
 });
 
 // ポート3000でサーバを立てる
