b1609dc21503a562be28bda38b23b0dd4838fcb4
t-mangoe <m.takashi.xpair@gmail.com>
2024-09-21 13:33:59 +0900

    add shebang to init.sh

--- init_elasticsearch/init.sh
+++ init_elasticsearch/init.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 curl -X POST http://localhost:9200/my_index/my_type?pretty -H "Content-Type":"application/json" -d @data.json
 curl -X POST http://localhost:9200/tags/tag_type?pretty -H "Content-Type":"application/json" -d @init_tags.json
